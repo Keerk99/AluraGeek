c014274a42bf5f51e00ec4a939d2c77d19f1ce9b
Keerk99 <antonyhuamantuanama@gmail.com>
2023-09-03 00:33:55 -0500

    Recongirando con MockApi

--- service/admin-service.js
+++ service/admin-service.js
@@ -1,8 +1,8 @@
 
-const listProducts = () => fetch("http://localhost:3000/api/product").then(respuesta => respuesta.json());
+const listProducts = () => fetch("https://64d425ac67b2662bf3dd0029.mockapi.io/product").then(respuesta => respuesta.json());
 
 const createProduct = (img, categoria, nombre, precio, description) => {
-    return fetch("http://localhost:3000/api/product", {
+    return fetch("https://64d425ac67b2662bf3dd0029.mockapi.io/product", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -12,15 +12,15 @@ const createProduct = (img, categoria, nombre, precio, description) => {
 }
 
 const deleteProduct = (id) => {
-    return fetch(`http://localhost:3000/api/product/${id}`, {
+    return fetch(`https://64d425ac67b2662bf3dd0029.mockapi.io/product/${id}`, {
         method: "DELETE",
     })
 }
 
-const detailProduct = (id) => fetch(`http://localhost:3000/api/product/${id}`).then(respuesta => respuesta.json());
+const detailProduct = (id) => fetch(`https://64d425ac67b2662bf3dd0029.mockapi.io/product/${id}`).then(respuesta => respuesta.json());
 
 const updateProduct = (img, categoria, nombre, precio, description, id) => {
-    return fetch(`http://localhost:3000/api/product/${id}`, {
+    return fetch(`https://64d425ac67b2662bf3dd0029.mockapi.io/product/${id}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
--- service/client-service.js
+++ service/client-service.js
@@ -1,6 +1,6 @@
-const listProduct = () => fetch("http://localhost:3000/api/product").then(respuesta => respuesta.json());
+const listProduct = () => fetch("https://64d425ac67b2662bf3dd0029.mockapi.io/product").then(respuesta => respuesta.json());
 
-const productDetail = (id) => fetch(`http://localhost:3000/api/product/${id}`).then(respuesta => respuesta.json());
+const productDetail = (id) => fetch(`https://64d425ac67b2662bf3dd0029.mockapi.io/product/${id}`).then(respuesta => respuesta.json());
 
 export const clientServices = {
     listProduct,
